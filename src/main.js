9ec514a37d616a73c02f7bcd45f81cd25db65e90
Jinrui Chen <caffreygo@163.com>
2020-04-02 09:54:30 +0800

    smalll fix

--- src/main.js
+++ src/main.js
@@ -3,7 +3,6 @@ import router from './router'
 import store from './store'
 import './plugins/axios'
 import ELEMENT from 'element-ui'
-// import 'element-ui/lib/theme-chalk/index.css';
 import App from './App.vue'
 import 'babel-polyfill'
 import i18n from './plugins/i18n'
